c192e4f77cafa714830ffd1e0a2913516c0c3be4
Lucio Paiva <luciopaiva@gmail.com>
2018-11-03 20:52:45 -0300

    Stars

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const NUM_STARS = 1000;
 const NUM_PARTICLES_SURFACE = 10000;
 const NUM_PARTICLES_SKY = 10000;
 const NUM_PARTICLES_FIRST_RING = 2000;
@@ -7,6 +8,7 @@ const TAU = Math.PI * 2;
 const ROT_PERIOD_IN_SECS = 30;
 const RADIUS = 250;
 const ATMOSPHERE_THICKNESS = 0.04;  // radius percent
+const ATMOSPHERE_RADIUS = RADIUS * (1 + ATMOSPHERE_THICKNESS);
 const LON_STEP = TAU / 60 / ROT_PERIOD_IN_SECS;  // 1 rotation per second
 const SURFACE_LON_STEP = LON_STEP * 0.6;
 const SKY_LAT_STEP = LON_STEP * 0.4;
@@ -29,12 +31,13 @@ class Globe {
 
         this.radius = RADIUS;
         this.lightSource = {x : .40, y : 0.40, z: 0.20};
+        this.stars = new Uint16Array(2 * NUM_STARS);
         this.particlesSky = new Float32Array(3 * NUM_PARTICLES_SKY);
         this.particlesSurface = new Float32Array(3 * NUM_PARTICLES_SURFACE);
         this.particlesFirstRing = new Float32Array(3 * NUM_PARTICLES_FIRST_RING);
         this.particlesSecondRing = new Float32Array(3 * NUM_PARTICLES_SECOND_RING);
 
-        for (let i = 0; i < NUM_PARTICLES_SURFACE * 2; i++) {
+        for (let i = 0; i < NUM_PARTICLES_SURFACE; i++) {
             this.particlesSurface[i * 3] = randomRads();
             this.particlesSurface[i * 3 + 1] = randomRads();
             this.particlesSurface[i * 3 + 2] = this.radius;
@@ -43,7 +46,7 @@ class Globe {
         for (let i = 0; i < NUM_PARTICLES_SKY; i++) {
             this.particlesSky[i * 3] = randomRads();
             this.particlesSky[i * 3 + 1] = randomRads();
-            this.particlesSky[i * 3 + 2] = this.radius + ATMOSPHERE_THICKNESS * RADIUS;
+            this.particlesSky[i * 3 + 2] = ATMOSPHERE_RADIUS;
         }
 
         for (let i = 0; i < NUM_PARTICLES_FIRST_RING; i++) {
@@ -61,6 +64,11 @@ class Globe {
         window.addEventListener("resize", this.resize.bind(this));
         this.resize();
         this.updateFn = this.update.bind(this);
+
+        for (let i = 0; i < NUM_STARS; i++) {
+            this.stars[i * 2] = Math.random() * this.width | 0;
+            this.stars[i * 2 + 1] = Math.random() * this.height | 0;
+        }
 
         document.body.appendChild(this.canvas);
 
@@ -143,11 +151,19 @@ class Globe {
     update(now) {
         this.ctx.clearRect(0, 0, this.width, this.height);
 
+        for (let i = 0; i < NUM_STARS; i++) {
+            const brightness = 15 + Math.random() * 10;
+            this.ctx.fillStyle = `hsl(0, 0%, ${brightness}%)`;
+            this.ctx.fillRect(this.stars[i * 2], this.stars[i * 2 + 1], 1, 1);
+        }
+
         // draw dark circle to represent the globe
         this.ctx.strokeStyle = "#020202";
+        this.ctx.fillStyle = "black";
         this.ctx.lineWidth = 1.5;
         this.ctx.beginPath();
-        this.ctx.arc(this.halfWidth, this.halfHeight, this.radius, 0, TAU);
+        this.ctx.arc(this.halfWidth, this.halfHeight, ATMOSPHERE_RADIUS, 0, TAU);
+        this.ctx.fill();
         this.ctx.stroke();
 
         this.drawParticles(this.particlesSurface, NUM_PARTICLES_SURFACE, 0, SURFACE_LON_STEP, 20, 100, 5, 50, 1);
