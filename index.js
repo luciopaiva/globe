a173a924af17b6ba053ca0887689bf78f92e28a1
Lucio Paiva <luciopaiva@gmail.com>
2018-11-03 18:55:02 -0300

    Crazy oscillation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-const NUM_PARTICLES = 1000;
+const NUM_PARTICLES = 10000;
 const TAU = Math.PI * 2;
-const ROT_PERIOD_IN_SECS = 6;
+const ROT_PERIOD_IN_SECS = 30;
+const RADIUS = 300;
 const LON_STEP = TAU / 60 / ROT_PERIOD_IN_SECS;  // 1 rotation per second
 
 const randomRads = () => Math.random() * TAU;
@@ -14,12 +15,8 @@ class Globe {
 
         this.ctx.clearRect(0, 0, this.width, this.height);
 
-        window.addEventListener("resize", this.resize.bind(this));
-        this.resize();
-
-        this.updateFn = this.update.bind(this);
-
-        this.radius = 250;
+        this.radius = RADIUS;
+        this.lightSource = {x : .40, y : 0.30, z: 0.30};
         this.particles = new Float32Array(3 * NUM_PARTICLES);
 
         for (let i = 0; i < NUM_PARTICLES; i++) {
@@ -27,6 +24,10 @@ class Globe {
             this.particles[i * 3 + 1] = randomRads();
             this.particles[i * 3 + 2] = this.radius;
         }
+
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+        this.updateFn = this.update.bind(this);
 
         document.body.appendChild(this.canvas);
 
@@ -44,17 +45,16 @@ class Globe {
     }
 
     update(now) {
-        this.previousTimestamp = now;
         this.ctx.clearRect(0, 0, this.width, this.height);
 
         // draw dark circle to represent the globe
-        this.ctx.fillStyle = "#140006";
-        this.ctx.beginPath();
-        this.ctx.arc(this.halfWidth, this.halfHeight, this.radius, 0, TAU);
-        this.ctx.fill();
+        // this.ctx.fillStyle = "#000000";
+        // this.ctx.beginPath();
+        // this.ctx.arc(this.halfWidth, this.halfHeight, this.radius, 0, TAU);
+        // this.ctx.fill();
 
+        let min = Number.POSITIVE_INFINITY, max = Number.NEGATIVE_INFINITY;
         // draw particles on the visible side
-        this.ctx.fillStyle = "#d20043";
         for (let i = 0; i < NUM_PARTICLES; i++) {
             const lat = this.particles[i * 3];
             const lon = this.particles[i * 3 + 1] + LON_STEP;
@@ -62,21 +62,45 @@ class Globe {
             // move particle
             this.particles[i * 3 + 1] = lon;
 
-            const elevation = this.particles[i * 3 + 2];
+            const elevation = this.particles[i * 3 + 2] + Math.cos(lat + lon + now / 500) * 0.5 * RADIUS;
 
-            const x = Math.cos(lat) * Math.cos(lon) * elevation | 0;
+            const x = Math.cos(lat) * Math.cos(lon) * elevation;
 
             // only show particle if it's facing the camera
             if (x > 0) {
                 // consider y to be x and z to be y (camera is seeing the sphere rotating from the side)
-                const y = (Math.cos(lat) * Math.sin(lon) * elevation + this.halfWidth) | 0;
-                const z = (this.halfHeight - Math.sin(lat) * elevation) | 0;
+                const y = Math.cos(lat) * Math.sin(lon) * elevation;
+                const z = Math.sin(lat) * elevation;
 
-                this.ctx.fillRect(y, z, 1, 1);
+                const intensity = this.calculateParticleIntensity(x, y, z);
+
+                const lightness = 5 + intensity * 40;
+
+                if (lightness < min) min = lightness;
+                if (lightness > max) max = lightness;
+
+                const hue = 220 + (360 - 220) * intensity;
+
+                this.ctx.fillStyle = `hsl(${hue}, 100%, ${lightness}%)`;  // 340
+
+                this.ctx.fillRect(y + this.halfWidth | 0, this.halfHeight - z | 0, 1, 1);
             }
         }
 
+        if (!this.sample) {
+            this.sample = true;
+            console.info(min, max);
+        }
+
+        this.previousTimestamp = now;
         requestAnimationFrame(this.updateFn);
+    }
+
+    calculateParticleIntensity(x, y, z) {
+        const len = Math.sqrt(x**2 + y**2 + z**2);
+        x /= len; y /=len; z /= len;
+        const dot = x * this.lightSource.x + y * this.lightSource.y + z * this.lightSource.z;
+        return dot;
     }
 }
 
