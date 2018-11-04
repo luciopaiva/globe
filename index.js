22ccae791b8cb565155b473378bb757e0e19c29a
Lucio Paiva <luciopaiva@gmail.com>
2018-11-03 22:14:35 -0300

    Satellite

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const FIRST_RING_INNER_RADIUS = RADIUS * 1.55;
 const FIRST_RING_THICKNESS = RADIUS * .10;
 const SECOND_RING_INNER_RADIUS = RADIUS * 1.70;
 const SECOND_RING_THICKNESS = RADIUS * .75;
+const SATELLITE_RADIUS = RADIUS * 1.25;
 const SHADES_OF_GRAY = false;
 
 const randomRads = () => Math.random() * TAU;
@@ -24,6 +25,9 @@ const randomRads = () => Math.random() * TAU;
 class Globe {
 
     constructor () {
+        this.satElem = document.getElementById("satellite-status");
+        this.fpsElem = document.getElementById("fps");
+
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext("2d");
 
@@ -32,6 +36,7 @@ class Globe {
         this.radius = RADIUS;
         this.lightSource = {x : .40, y : 0.40, z: 0.20};
         this.stars = new Uint16Array(2 * NUM_STARS);
+        this.satellite = new Float32Array(3);
         this.particlesSky = new Float32Array(3 * NUM_PARTICLES_SKY);
         this.particlesSurface = new Float32Array(3 * NUM_PARTICLES_SURFACE);
         this.particlesFirstRing = new Float32Array(3 * NUM_PARTICLES_FIRST_RING);
@@ -60,6 +65,13 @@ class Globe {
             this.particlesSecondRing[i * 3 + 1] = randomRads();
             this.particlesSecondRing[i * 3 + 2] = SECOND_RING_INNER_RADIUS + Math.random() * SECOND_RING_THICKNESS;
         }
+
+        this.satellite[0] = 0;
+        this.satellite[1] = 0;
+        this.satellite[2] = SATELLITE_RADIUS;
+
+        this.fpsCount = 0;
+        setInterval(() => { this.fpsElem.innerText = "FPS: " + this.fpsCount; this.fpsCount = 0; }, 1000);
 
         window.addEventListener("resize", this.resize.bind(this));
         this.resize();
@@ -110,7 +122,8 @@ class Globe {
         ];
     }
 
-    drawParticles(particles, particlesLength, latStep, lonStep, hue, saturation, baseLightness, lightnessBand, particleSize, isRing) {
+    drawParticles(now, particles, particlesLength, latStep, lonStep, hue, saturation, baseLightness, lightnessBand,
+                  particleSize, isOrbiting, isSatellite) {
         for (let i = 0; i < particlesLength; i++) {
             const lat = particles[i * 3] + latStep;
             const lon = particles[i * 3 + 1] + lonStep;
@@ -124,26 +137,39 @@ class Globe {
             let x = Math.cos(lat) * Math.cos(lon) * elevation;
 
             // only show particle if it's facing the camera (or if particle belongs to ring)
-            if (x > 0 || isRing) {
+            if (x > 0 || isOrbiting) {
                 // consider y to be x and z to be y (camera is seeing the sphere rotating from the side)
                 let y = Math.cos(lat) * Math.sin(lon) * elevation;
                 let z = Math.sin(lat) * elevation;
 
-                if (isRing) {
+                if (isSatellite) {
+                    this.satElem.innerText = `Satellite coords: ${x.toFixed(0)}, ${y.toFixed(0)}, ${z.toFixed(0)}`;
+                }
+
+                if (isOrbiting) {
                     [x, y, z] = this.rotateY(x, y, z, -Math.PI * .05);
                     [x, y, z] = this.rotateX(x, y, z, Math.PI * .05);
                 }
 
                 // [x, y, z] = this.project(x, y, z);
 
-                const intensity = this.calculateParticleIntensity(x, y, z);
+                let intensity;
+                if (isSatellite) {
+                    intensity = (Math.cos(now / 100) + 1) / 2;
+                } else {
+                    intensity = this.calculateParticleIntensity(x, y, z);
+                }
 
                 const lightness = baseLightness + intensity * lightnessBand;
 
                 saturation = SHADES_OF_GRAY ? 0 : saturation;
                 this.ctx.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;  // 340
 
-                this.ctx.fillRect(y + this.halfWidth | 0, this.halfHeight - z | 0, particleSize, particleSize);
+                if (isSatellite && x < 0 && Math.abs(y) < RADIUS) {
+                    continue;  // do not render satellite if behind planet
+                }
+
+                this.ctx.fillRect(y + this.halfWidth, this.halfHeight - z, particleSize, particleSize);
             }
         }
     }
@@ -162,15 +188,17 @@ class Globe {
         this.ctx.fillStyle = "black";
         this.ctx.lineWidth = 1.5;
         this.ctx.beginPath();
-        this.ctx.arc(this.halfWidth, this.halfHeight, ATMOSPHERE_RADIUS, 0, TAU);
+        this.ctx.arc(this.halfWidth, this.halfHeight, RADIUS, 0, TAU);
         this.ctx.fill();
         this.ctx.stroke();
 
-        this.drawParticles(this.particlesSurface, NUM_PARTICLES_SURFACE, 0, SURFACE_LON_STEP, 20, 100, 5, 50, 1);
-        this.drawParticles(this.particlesSky, NUM_PARTICLES_SKY, SKY_LAT_STEP, SKY_LON_STEP, 30, 100, 5, 20, 0.7);
-        this.drawParticles(this.particlesFirstRing, NUM_PARTICLES_FIRST_RING, 0, SURFACE_LON_STEP, 320, 80, 5, 80, 0.7, true);
-        this.drawParticles(this.particlesSecondRing, NUM_PARTICLES_SECOND_RING, 0, SURFACE_LON_STEP, 300, 50, 5, 80, 0.7, true);
+        this.drawParticles(now, this.particlesSurface, NUM_PARTICLES_SURFACE, 0, SURFACE_LON_STEP, 20, 100, 5, 50, 1);
+        this.drawParticles(now, this.particlesSky, NUM_PARTICLES_SKY, SKY_LAT_STEP, SKY_LON_STEP, 30, 100, 5, 20, 0.7);
+        this.drawParticles(now, this.satellite, 1, 0, SKY_LON_STEP, 0, 100, 0, 60, 2, true, true);
+        this.drawParticles(now, this.particlesFirstRing, NUM_PARTICLES_FIRST_RING, 0, SURFACE_LON_STEP, 320, 80, 5, 80, 0.7, true);
+        this.drawParticles(now, this.particlesSecondRing, NUM_PARTICLES_SECOND_RING, 0, SURFACE_LON_STEP, 300, 50, 5, 80, 0.7, true);
 
+        this.fpsCount++;
         this.previousTimestamp = now;
         requestAnimationFrame(this.updateFn);
     }
